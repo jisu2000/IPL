619cd11a6891760f0eaa968436ec49088066f746
jisu2000 <spjisu2000@gmail.com>
2023-01-08 18:31:17 +0530

    Added

---
+++ src/Bowler.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Bowler extends Batsman {
+    private int overs;
+    private int wickets;
+
+    public Bowler() {
+    }
+
+
+    public Bowler(int playerId, String playerName, int runs, int hundreds, int fifties, int overs, int wickets) {
+        super(playerId, playerName, runs, hundreds, fifties);
+        this.overs = overs;
+        this.wickets = wickets;
+    }
+
+    public int getOvers() {
+        return overs;
+    }
+
+    public void setOvers(int overs) {
+        this.overs = overs;
+    }
+
+    public int getWickets() {
+        return wickets;
+    }
+
+    public void setWickets(int wickets) {
+        this.wickets = wickets;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ "  Bowler{" +
+                "overs=" + overs +
+                ", wickets=" + wickets +
+                '}';
+    }
+
+    public void read(){
+        super.read();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter overs and Wickets");
+        this.overs=sc.nextInt();
+        this.wickets=sc.nextInt();
+
+    }
+}
